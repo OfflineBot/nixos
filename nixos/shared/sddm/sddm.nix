5bd0123118ccfb0bbe8eb9f48891a2494fa14c72
OfflineBot <Flintstonelg@gmail.com>
2024-10-10 17:54:57 +0200

    sddm

--- nixos/shared/sddm/sddm.nix
+++ nixos/shared/sddm/sddm.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }:
 {
+    system.activationScripts.sddmThemeSymlink = ''
+        mkdir -p /usr/share/sddm/themes
+        ln -sfn ${./src} /usr/share/sddm/themes/sddm-sugar-candy
+    '';
+
     services.displayManager.sddm = {
         enable = true;
         theme = "/usr/share/sddm/themes/sddm-sugar-candy";
