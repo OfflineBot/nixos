5b640b2e116f6502643036e65d0d4f37845a3161
OfflineBot <FlintstoneLG@gmail.com>
2024-10-11 18:47:51 +0200

    theme

--- home-manager/shared/src/tmgr/tmgr.sh
+++ home-manager/shared/src/tmgr/tmgr.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
-echo "hello"
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <argument>"
+    exit 1
+fi
+
+# Assign the argument to a variable
+ACTION=$1
+
+# Perform actions based on the argument
+case $ACTION in
+    trans)
+        cp ~/.config/themes/themes/kitty_colors_transparent.conf ~/.config/themes/kitty_colors.conf
+        ;;
+    solid)
+        cp ~/.config/themes/themes/kitty_colors_solid.conf ~/.config/themes/kitty_colors.conf
+        ;;
+    *)
+        echo "invalid argument"
+        exit 1
+        ;;
+esac
