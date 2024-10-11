7b06319face7917976eae353067551ba549d8479
OfflineBot <FlintstoneLG@gmail.com>
2024-10-11 18:33:48 +0200

    tmgr

--- home-manager/shared/apps/themes.nix
+++ home-manager/shared/apps/themes.nix
@@ -6,7 +6,7 @@
         chmod -R 777 ~/.config/themes
     '';
 
-    home.activation.themesFolder = lib.mkAfter ''
+    home.activation.themesTmgr = lib.mkAfter ''
         mkdir -p ~/Apps
         cp -r ${../src/theme_mgr/tmgr.sh} ~/Apps/tmgr.sh
         chmod +x ~/Apps/tmgr.sh
