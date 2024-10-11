296c5b41c56d026dcbcfd69c91b24aa7eae27867
OfflineBot <FlintstoneLG@gmail.com>
2024-10-11 16:50:42 +0200

    themes

--- home-manager/shared/apps/themes.nix
+++ home-manager/shared/apps/themes.nix
@@ -1,7 +1,12 @@
 { config, pkgs, lib, ... }:
 {
+    # themes folder
+
     home.activation.copyKittyConfig = pkgs.lib.mkAfter ''
         mkdir -p ${config.home.homeDirectory}/.config/themes
         cp -r ${../src/themes}/* ~/.config/themes
     '';
+
+    # themes manager
+    home.file."Apps/tm.sh".source = ../src/theme_manager/tm.sh;
 }
