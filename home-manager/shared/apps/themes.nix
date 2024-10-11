de0d481724215d6ccb4b3c67f46f48947e35ef6f
OfflineBot <FlintstoneLG@gmail.com>
2024-10-11 18:24:36 +0200

    theme

--- home-manager/shared/apps/themes.nix
+++ home-manager/shared/apps/themes.nix
@@ -1,7 +1,8 @@
 { config, pkgs, lib, ... }:
 {
-    home.activation.themesFolder = lib.mkAfter = ''
+    home.activation.themesFolder = lib.mkAfter ''
         mkdir -p ~/.config/themes
         cp -r ${../src/themes}/* ~/.config/themes
+        chmod -R 777 ~/.config/themes
     '';
 }
