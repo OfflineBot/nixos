96ae8d191e6290f5065faa13c9a503edbeef6d3d
OfflineBot <Flintstonelg@gmail.com>
2024-10-14 10:57:30 +0200

    default-transparent

--- home-manager/shared/src/tmgr/tmgr.sh
+++ home-manager/shared/src/tmgr/tmgr.sh
@@ -14,11 +14,17 @@ case $ACTION in
         cp ~/.config/themes/themes/kitty_colors_default.conf ~/.config/themes/kitty_colors.conf
         swww img ~/Pictures/active_wallpaper/darker_catpuccin.jpg
         hyprctl keyword general:col.active_border "rgba(8ee0d4ee) rgba(e69ea8ee) 45deg"
+        hyprctl keyword decoration:blur:enabled false
+        ;;
+    default-trans)
         ;;
     sakura)
         cp ~/.config/themes/themes/kitty_colors_sakura.conf ~/.config/themes/kitty_colors.conf
         swww img ~/Pictures/active_wallpaper/red_sakura.jpg
         hyprctl keyword general:col.active_border "rgba(d93840ee) rgba(731015ee) 45deg"
+        hyprctl keyword decoration:blur:enabled true
+        hyprctl keyword decoration:blur:size 10
+        hyprctl keyword decoration:blur:passes 2
         ;;
     *)
         echo "invalid argument"
