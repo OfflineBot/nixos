3e653c3395b69f350fb55ad94c3ab51fa42d6b30
OfflineBot <Flintstonelg@gmail.com>
2024-10-14 10:19:12 +0200

    tmgr

--- home-manager/shared/src/tmgr/tmgr.sh
+++ home-manager/shared/src/tmgr/tmgr.sh
@@ -13,10 +13,12 @@ case $ACTION in
     default)
         cp ~/.config/themes/themes/kitty_colors_default.conf ~/.config/themes/kitty_colors.conf
         swww img ~/Pictures/active_wallpaper/darker_catpuccin.jpg
+        hyprctl keyword general:col.active_border "rgba(8ee0d4ee) rgba(e69ea8ee) 45deg"
         ;;
     sakura)
         cp ~/.config/themes/themes/kitty_colors_sakura.conf ~/.config/themes/kitty_colors.conf
         swww img ~/Pictures/active_wallpaper/red_sakura.jpg
+        hyprctl keyword general:col.active_border "rgba(d93840ee) rgba(731015ee) 45deg"
         ;;
     *)
         echo "invalid argument"
