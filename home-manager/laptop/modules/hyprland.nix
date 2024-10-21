0402001fe95d14676ba123ae41f58e7bcc7bb65a
OfflineBot <FlintstoneLG@gmail.com>
2024-10-21 17:50:33 +0200

    idk :/

--- home-manager/laptop/modules/hyprland.nix
+++ home-manager/laptop/modules/hyprland.nix
@@ -1,4 +1,7 @@
-{ ... }:
+{ inputs, pkgs, ... }:
 {
     home.file.".config/hypr".source = ../src/hyprland;
+    wayland.windowManager.hyprland.plugins = [
+        inputs.hyprland-plugins.packages.${pkgs.system}.borders-plus-plus
+    ];
 }
