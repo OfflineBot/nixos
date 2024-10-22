e84980ce54d4c305b1181ba191c8f276ac283615
OfflineBot <FlintstoneLG@gmail.com>
2024-10-22 20:18:08 +0200

    better path

--- home-manager/shared/apps/kitty.nix
+++ home-manager/shared/apps/kitty.nix
@@ -1,5 +1,5 @@
 { config, pkgs, lib, ... }: 
 {
     #home.file.".config/kitty/".source = config.lib.file.mkOutOfStoreSymlink ../src/kitty;
-    home.file.".config/kitty/kitty.conf".source = config.lib.file.mkOutOfStoreSymlink /home/offlinebot/System2/home-manager/shared/src/kitty/kitty.conf;
+    home.file.".config/kitty/kitty.conf".source = config.lib.file.mkOutOfStoreSymlink config.home.homeDirectory/shared/src/kitty/kitty.conf;
 }
