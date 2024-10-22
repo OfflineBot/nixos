25413750e1c97cf9883609fcc9922d7b61a90c60
OfflineBot <Flintstonelg@gmail.com>
2024-10-22 20:11:46 +0200

    kitty working (not perfect yet with path)

--- home-manager/shared/apps/kitty.nix
+++ home-manager/shared/apps/kitty.nix
@@ -1,5 +1,5 @@
 { config, pkgs, lib, ... }: 
 {
-    #programs.kitty.enable = true;
-    home.file.".config/kitty/".source = config.lib.file.mkOutOfStoreSymlink ../src/kitty;
+    #home.file.".config/kitty/".source = config.lib.file.mkOutOfStoreSymlink ../src/kitty;
+    home.file.".config/kitty/kitty.conf".source = config.lib.file.mkOutOfStoreSymlink /home/offlinebot/System2/home-manager/shared/src/kitty/kitty.conf;
 }
