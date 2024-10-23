fcbbe02f592de68ccc7b30deb7000909a973ab49
OfflineBot <Flintstonelg@gmail.com>
2024-10-23 14:51:27 +0200

    new

--- home-manager/shared/src/hypr/hypr_pc/scripts/main.py
+++ home-manager/shared/src/hypr/hypr_pc/scripts/main.py
@@ -7,12 +7,15 @@ import subprocess
 
 user_path = '/home/offlinebot/'
 impage_path = 'Pictures/other_wallpaper/'
+kitty_path = '.config/kitty/'
 full_image_path = os.path.join(user_path, impage_path)
+full_kitty_path = os.path.join(user_path, kitty_path)
 
 #struct = {
 #    "maximum": int,
 #    "current": int,
 #}
+
 
 def handle_files(path: str):
     cyan    =   "rgba(52c7c5ee)"
@@ -25,15 +28,19 @@ def handle_files(path: str):
     if "cyan" in path:
         hyprctl_command.append(cyan)
         subprocess.run(hyprctl_command)
+        subprocess.run(["cp", os.path.join(full_kitty_path, 'colors/cyan.conf'), os.path.join(full_kitty_path, 'colors.conf')])
     elif "red" in path:
         hyprctl_command.append(red)
         subprocess.run(hyprctl_command)
+        subprocess.run(["cp", os.path.join(full_kitty_path, 'colors/red.conf'), os.path.join(full_kitty_path, 'colors.conf')])
     elif "purple" in path:
         hyprctl_command.append(purple)
         subprocess.run(hyprctl_command)
+        subprocess.run(["cp", os.path.join(full_kitty_path, 'colors/purple.conf'), os.path.join(full_kitty_path, 'colors.conf')])
     else:
         hyprctl_command.append(white)
         subprocess.run(hyprctl_command)
+        subprocess.run(["cp", os.path.join(full_kitty_path, 'colors/default.conf'), os.path.join(full_kitty_path, 'colors.conf')])
 
     subprocess.run(["swww", "img", os.path.join(full_image_path, path)], check=True)
 
