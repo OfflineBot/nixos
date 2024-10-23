e933c2eb5f08936e44b09b7227648d1d455e0c8b
OfflineBot <Flintstonelg@gmail.com>
2024-10-23 11:55:28 +0200

    ags works

--- home-manager/laptop/laptop.nix
+++ home-manager/laptop/laptop.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, inputs, ... }:
 let 
     x = "laptop";
 in
@@ -8,7 +8,7 @@ in
         ../shared/default.nix
         #./modules/default.nix
         (import ../shared/special_apps/hyprland2.nix { inherit config pkgs x; })
-        (import ../shared/special_apps/ags.nix { inherit config pkgs x; })
+        (import ../shared/special_apps/ags.nix { inherit config lib inputs pkgs x; })
     ];
 
 }
--- home-manager/pc/pc.nix
+++ home-manager/pc/pc.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, inputs, ... }:
 let 
     x = "pc";
 in
@@ -7,6 +7,6 @@ in
         ../shared/default.nix
         #./modules/default.nix
         (import ../shared/special_apps/hyprland2.nix { inherit config pkgs x; })
-        (import ../shared/special_apps/ags.nix { inherit config pkgs x; })
+        (import ../shared/special_apps/ags.nix { inherit config lib inputs pkgs x; })
     ];
 }
