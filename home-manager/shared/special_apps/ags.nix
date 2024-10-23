cc13ed9bd3797ffd5b320a173a331e1aa08a4c2b
OfflineBot <Flintstonelg@gmail.com>
2024-10-23 11:53:30 +0200

    ags should work again

--- home-manager/shared/special_apps/ags.nix
+++ home-manager/shared/special_apps/ags.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, x ? "none", ... }:
+{ config, lib, inputs, pkgs, x ? "none", ... }:
 let
     homeManagerDir = "${config.home.homeDirectory}/System2/home-manager";
     agsConfig = if x == "laptop" then
@@ -7,5 +7,20 @@ let
         "${homeManagerDir}/shared/src/ags/ags_pc";
 in
 {
+    imports = [ 
+        inputs.ags.homeManagerModules.default 
+    ];
+
+    programs.ags = {
+        enable = true;  # Enable AGS
+
+        # Additional packages to add to gjs's runtime
+        extraPackages = with pkgs; [
+            gtksourceview
+            webkitgtk
+            accountsservice
+        ];
+    };
+
     home.file.".config/ags".source = config.lib.file.mkOutOfStoreSymlink agsConfig;
 }
