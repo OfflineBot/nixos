59f2b34bf6f373383a73bb175838e6a13a6a33d8
OfflineBot <Flintstonelg@gmail.com>
2024-10-23 10:08:49 +0200

    laptop hypr compact

--- home-manager/laptop/laptop.nix
+++ home-manager/laptop/laptop.nix
@@ -1,9 +1,13 @@
-{ config, ... }:
+{ config, pkgs, ... }:
+let 
+    x = "laptop";
+in
 {
 
     imports = [ 
         ../shared/default.nix
         ./modules/default.nix
+        (import ../shared/special_apps/hyprland.nix { inherit config pkgs x; })
     ];
 
 }
